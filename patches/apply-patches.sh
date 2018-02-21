390613a4963e0657df7fc918fd1fa2e7f9fec645
rote66 <pontifex23872388@gmail.com>
2018-02-21 17:46:45 +0800

    fix poweroff and other

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -4,6 +4,6 @@ cd system/sepolicy
 git apply --ignore-space-change --ignore-whitespace -v ../../device/Xiaomi/HM2014501/patches/system_sepolicy/0001-Revert-back-to-version-29.patch
 cd ../..
 cd bootable/recovery
-git apply --ignore-space-change --ignore-whitespace -v ../../device/Xiaomi/HM2014501/patches/bootable/recovery/0001-fix-reboot-and-blank.patch
+git apply --ignore-space-change --ignore-whitespace -v ../../device/Xiaomi/HM2014501/patches/bootable_recovery/0001-fix-reboot-and-blank.patch
 cd ../..
 echo Patches Applied Successfully!
